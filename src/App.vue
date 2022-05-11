d4e26fc8129abde4b9402b865504fea3d923ce3c
Brayan Iribe <brayaniribe@gmail.com>
2022-05-10 19:18:07 -0700

    Agregado footer

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     />
     <div class="row" v-if="isLogin">
       <div class="col-sm-8">
-        <h2 class="mb-1">Minera</h2>
+        <h2 class="text-pink mb-1">Minera</h2>
         <p class="mb-4" style="color: #7c7a7a; line-height: 1rem">
           Conozca en tiempo real el estado del sistema MVTS / Map View Traffic
           System
@@ -26,7 +26,7 @@
         <p v-if="updatedAt">Actualizado localmente el {{ updatedAt }}</p>
       </div>
       <div class="col-sm-4">
-        <h2 class="mb-1">Semáforos</h2>
+        <h2 class="text-pink mb-1">Semáforos</h2>
         <p style="color: #7c7a7a; line-height: 1rem">
           Haga clic sobre los semáforos en el mapa o en los botones para cambiar
           su estado.
@@ -38,7 +38,7 @@
           :data="semaforo"
           @onClickSemaforo="actualizarSemaforo"
         />
-        <h2 class="mb-1 mt-4 d-block">Vehículos</h2>
+        <h2 class="text-pink mb-1 mt-4 d-block">Vehículos</h2>
         <p style="color: #7c7a7a; line-height: 1rem">
           Conozca el estado de cada vehículo, en tiempo real.
         </p>
@@ -48,6 +48,18 @@
           :key="'vehiculo_card_' + vehiculo.idVehiculo"
           :data="vehiculo"
         />
+      </div>
+      <div class="col-sm-12">
+        <p style="line-height: 1.2rem; color: #7c7a7a">
+          "Minera" es un proyecto del sexto semestre del ITSON, para las
+          materias de Sistemas Distribuidos y Arquitecturas Empresariales. En
+          este proyecto se empleó la arquitectura Cliente-Servidor con MVC,
+          utilizando Vue.js en el frontend y Spring boot Java en el backend. Una
+          aplicación móvil para Android, desarrollada en Kotlin es utilizada
+          para registrar la ubicación de cada vehículo en tiempo real. Los
+          integrantes del equipo son Brayan Iribe 216663, Miguel Sanez 216756 y
+          Andrés Urías 216769.
+        </p>
       </div>
     </div>
   </div>
@@ -140,6 +152,9 @@ body {
 .i-title {
   font-size: 4rem;
   font-style: italic;
+}
+
+.text-pink {
   color: #ff5d73;
 }
 
